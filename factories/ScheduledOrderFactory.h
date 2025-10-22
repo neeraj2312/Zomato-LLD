156f297763ff2eb2ce7734e019d0dcdbb1cc15d5
Code With Neeraj <133529669+neeraj2312@users.noreply.github.com>
2025-10-23 00:03:54 +0530

    Add files via upload

--- factories/ScheduledOrderFactory.h
+++ factories/ScheduledOrderFactory.h
@@ -1,5 +1,5 @@
-#ifndef NOW_ORDER_FACTORY_H
-#define NOW_ORDER_FACTORY_H
+#ifndef SCHEDULED_ORDER_FACTORY_H
+#define SCHEDULED_ORDER_FACTORY_H
 
 #include "OrderFactory.h"
 #include "../models/DeliveryOrder.h"
@@ -7,29 +7,35 @@
 #include "../utils/TimeUtils.h"
 using namespace std;
 
-class NowOrderFactory : public OrderFactory {
+class ScheduledOrderFactory : public OrderFactory {
+private:
+    string scheduleTime;
 public:
+    ScheduledOrderFactory(string scheduleTime) {
+        this->scheduleTime = scheduleTime;
+    }
+
     Order* createOrder(User* user, Cart* cart, Restaurant* restaurant, const vector<MenuItem>& menuItems,
-                       PaymentStrategy* paymentStrategy, double totalCost, const string& orderType) override {
+                        PaymentStrategy* paymentStrategy, double totalCost, const string& orderType) override {
         Order* order = nullptr;
-        if (orderType == "Delivery") {
+
+        if(orderType == "Delivery") {
             auto deliveryOrder = new DeliveryOrder();
             deliveryOrder->setUserAddress(user->getAddress());
             order = deliveryOrder;
-        }
+        } 
         else {
             auto pickupOrder = new PickupOrder();
             pickupOrder->setRestaurantAddress(restaurant->getLocation());
-            order = pickupOrder;
         }
         order->setUser(user);
         order->setRestaurant(restaurant);
         order->setItems(menuItems);
         order->setPaymentStrategy(paymentStrategy);
-        order->setScheduled(TimeUtils::getCurrentTime());
+        order->setScheduled(scheduleTime);
         order->setTotal(totalCost);
         return order;
     }
 };
 
-#endif // NOW_ORDER_FACTORY_H
+#endif // SCHEDULED_ORDER_FACTORY_H
